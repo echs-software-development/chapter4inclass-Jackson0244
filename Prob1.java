6a536bc182d7858eb01611a44d0133cf4d45ba42
JACKSON GERBES <jg0244@students.necsd.net>
2021-01-04 03:09:37 +0000

    the code

--- Prob1.java
+++ Prob1.java
@@ -1,8 +1,30 @@
-
 import java.util.Scanner;
+import edhesive.testing.Math;
 
-public class Prob1 {
+public class chapt4problem1{
   public static void main(String[] args){
 
+    /* Write your code here */
+  Scanner scan = new Scanner(System.in);
+  
+  System.out.println("Enter a positive integer.");
+  int n = scan.nextInt();
+
+  double ran = Math.random();
+  int range = n;
+  int min = 1;
+  int answer = (int) (ran * range) + min;
+  
+  System.out.println("Printing 10 random integers from 1 to n...");
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
   }
 }
--- Prob3.java
+++ Prob3.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 
-public class Prob3 {
-  public static void main(String[] args) {
-    
+public class Exercise_04_03 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
+		// Prompt the user to enter an ASCII code
+		// (an integer between 0 and 127)
+		System.out.print("Enter an ASCII code: ");
+		int i = input.nextInt();
 
-  }
+		// Display ASCII code as character
+		System.out.println((char)i);
+	}
 }
